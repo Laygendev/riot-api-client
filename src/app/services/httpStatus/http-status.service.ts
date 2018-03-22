08f343dc090618b4a70a04e127e545da0d3a1878
= <jimmy@eoxia.com>
2018-03-22 22:16:21 +0100

    1.0.0: Review https

--- src/app/services/httpStatus/http-status.service.ts
+++ src/app/services/httpStatus/http-status.service.ts
@@ -11,7 +11,7 @@ export class HttpStatusService {
 	constructor(private httpClient: HttpClient) {}
 
 	public get(): Observable<any> {
-		let url: string = "http://54.36.43.4:3002/status";
+		let url: string = "http://server.guideslol.com:3002/status";
 		return this.httpClient.get( url, {responseType: 'json'} );
 	}
 }
