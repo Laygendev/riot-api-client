ae794d5f16dcc220f0067d8fa44d7d50a609af2c
eoxia-jimmy <jimmy.eoxia@gmail.com>
2018-03-28 17:39:45 +0200

    1.0.0: Change URL

--- src/app/services/httpUser/http-user.service.ts
+++ src/app/services/httpUser/http-user.service.ts
@@ -13,23 +13,23 @@ export class HttpUserService {
 	constructor(private httpClient: HttpClient) {}
 
 	public get(args: any): Observable<any> {
-		let url: string = "http://54.36.43.4:3002/user/" + args.email + "/" + args.pwd;
+		let url: string = "https://server.guideslol.com/user/" + args.email + "/" + args.pwd;
 
 		if ( args._id ) {
-			url = "http://54.36.43.4:3002/user/" + args._id;
+			url = "https://server.guideslol.com/user/" + args._id;
 		}
 
 		return this.httpClient.get( url, {responseType: 'json'} );
 	}
 
 	public post(data: UserModel): Observable<any> {
-		let url: string = "http://54.36.43.4:3002/user/";
+		let url: string = "https://server.guideslol.com/user/";
 
 		return this.httpClient.post( url, data, {responseType: 'json'} );
 	}
 
 	public auth(data: UserModel): Observable<any> {
-		let url: string = "http://54.36.43.4:3002/user/auth";
+		let url: string = "https://server.guideslol.com/user/auth";
 
 		return this.httpClient.post( url, data, {responseType: 'json'} );
 	}
