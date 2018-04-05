dd378121a48bec46388631840212c9dbbf3396e2
eoxia-jimmy <jimmy.eoxia@gmail.com>
2018-04-05 17:44:06 +0200

    1.1.0: Fix error

--- src/app/directives/search/search.directive.spec.ts
+++ src/app/directives/search/search.directive.spec.ts
@@ -2,7 +2,7 @@ import { SearchDirective } from './search.directive';
 
 describe('SearchDirective', () => {
   it('should create an instance', () => {
-    const directive = new SearchDirective();
+    const directive = new SearchDirective(undefined);
     expect(directive).toBeTruthy();
   });
 });
