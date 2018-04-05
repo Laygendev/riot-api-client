4283f2dee608a388658b8f913ebed052400d6850
eoxia-jimmy <jimmy.eoxia@gmail.com>
2018-04-05 15:31:01 +0200

    1.1.0: Add platform detection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -107,4 +107,12 @@ const cookieConfig:NgcCookieConsentConfig = {
 	],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(APP_ID) private appId: string) {
+    const platform = isPlatformBrowser(platformId) ?
+      'in the browser' : 'on the server';
+    console.log(`Running ${platform} with appId=${appId}`);
+  }
+}
