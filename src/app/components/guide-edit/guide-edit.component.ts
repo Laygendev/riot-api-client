fc91f83fa1f366630a9182d712a3ea6f8077db8d
eoxia-jimmy <jimmy.eoxia@gmail.com>
2018-04-18 13:16:21 +0200

    1.1.0: Fix background

--- src/app/components/guide-edit/guide-edit.component.ts
+++ src/app/components/guide-edit/guide-edit.component.ts
@@ -140,7 +140,7 @@ export class GuideEditComponent implements OnInit {
 
 	getUrl(): string {
 		if (this.champion) {
-			return "url('http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + this.champion.name + "_0.jpg')";
+			return "url('http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + this.champion.name.replace( ' ', '' ) + "_0.jpg')";
 		}
 	}
 }
